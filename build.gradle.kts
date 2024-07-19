88a8aa43755c44f87cc43f7fe5c58f67ebd0e604
shipdoorwater <baemoonsu89@gmail.com>
2024-07-19 18:41:14 +0900

    지도추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,13 @@ plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
 }
+
+
+// 네이버 지도 SDK 저장소 설정 추가 -< Maven 저장소에서 배포
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://repository.map.naver.com/archive/maven")
+    }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,13 +9,16 @@ pluginManagement {
         }
         mavenCentral()
         gradlePluginPortal()
+        maven { url = uri("https://naver.jfrog.io/artifactory/maven/") }
     }
 }
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    //  프로젝트레벨에서 레포지토리를 설정하지 않도록 주석 처리
+    //    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://naver.jfrog.io/artifactory/maven/") }
     }
 }
 
