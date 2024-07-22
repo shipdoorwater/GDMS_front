af35e394cb2f9a6728afe4aaf67cfbc2374bc4dc
sangjoon88 <ramax@naver.com>
2024-07-22 12:42:27 +0900

    로그인

---
+++ app/src/main/java/com/example/gdms_front/auth/LoginRequest.kt
@@ -0,0 +1,8 @@
+package com.example.gdms_front.auth
+
+import android.media.session.MediaSession.Token
+
+data class LoginRequest (
+    val userId : String,
+    val userPw : String
+)
---
+++ app/src/main/java/com/example/gdms_front/auth/LoginResponse.kt
@@ -0,0 +1,5 @@
+package com.example.gdms_front.auth
+
+data class LoginResponse (
+    val token : String
+)
