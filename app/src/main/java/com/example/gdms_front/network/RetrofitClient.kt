9bf46b2d8d38348e9b4d6566ec0ca49b1e3ff9f7
shipdoorwater <baemoonsu89@gmail.com>
2024-07-23 17:41:45 +0900

    마커용api 추가중

--- app/src/main/java/com/example/gdms_front/network/RetrofitClient.kt
+++ app/src/main/java/com/example/gdms_front/network/RetrofitClient.kt
@@ -27,4 +27,13 @@ object RetrofitClient {
             .build()
             .create(ApiService::class.java)
     }
+
+    val mapApiService: MapApiService by lazy {
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+
+        retrofit.create(MapApiService::class.java)
+    }
 }
