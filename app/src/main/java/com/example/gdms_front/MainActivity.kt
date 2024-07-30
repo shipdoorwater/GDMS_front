991cb224c2e343ff1174427b47db1b4a8d9d2b24
sangjoon88 <ramax@naver.com>
2024-07-30 17:49:50 +0900

    구독 계속계속계속 작업 중

--- app/src/main/java/com/example/gdms_front/MainActivity.kt
+++ app/src/main/java/com/example/gdms_front/MainActivity.kt
@@ -2,12 +2,14 @@ package com.example.gdms_front
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.gdms_front.myPage.MyPageActivity
+import com.example.gdms_front.navigation_frag.ProfitFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +19,18 @@ class MainActivity : AppCompatActivity() {
         findViewById<ImageView>(R.id.myPageBtn).setOnClickListener {
             val intent = Intent(this, MyPageActivity::class.java)
             startActivity(intent)
+        }
+        
+        
+        // 다른 엑티비티에서 나와서 지울 때
+        val fragmentToLoad = intent.getStringExtra("FragmentToLoad")
+        Log.d("FragmentToLoad", "FragmentToLoad: $fragmentToLoad")
+
+        if (fragmentToLoad=="ProfitFragment") {
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.fragmentContainerView, ProfitFragment())
+            .commit()
+            intent.removeExtra("FragmentToLoad")
         }
     }
 }
