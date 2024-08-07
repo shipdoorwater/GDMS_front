0db5cb14bfd3a1b81d985e3e5121d4421ec73531
sangjoon88 <ramax@naver.com>
2024-08-07 15:37:36 +0900

    디자인 추가

---
+++ app/src/main/java/com/example/gdms_front/adapter/CvStoreImageAdapter.kt
@@ -0,0 +1,32 @@
+package com.example.gdms_front.adapter
+
+import android.content.Context
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.gdms_front.R
+
+class CvStoreImageAdapter(private val context: Context, private val images: List<Int>) :
+    RecyclerView.Adapter<CvStoreImageAdapter.ImageViewHolder>() {
+
+    inner class ImageViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val imageView: ImageView = view.findViewById(R.id.imageView)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+        val view = LayoutInflater.from(context).inflate(R.layout.item_cvstore_image, parent, false)
+        return ImageViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        holder.imageView.setImageResource(images[position % images.size])
+    }
+
+    override fun getItemCount(): Int = Int.MAX_VALUE
+
+
+}
