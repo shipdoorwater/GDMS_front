d3877223f5e058e378a7eb456735f1343d69e504
shipdoorwater <baemoonsu89@gmail.com>
2024-08-16 17:52:29 +0900

    스플래쉬 수정

--- app/src/main/java/com/example/gdms_front/SplashActivity.kt
+++ app/src/main/java/com/example/gdms_front/SplashActivity.kt
@@ -1,6 +1,8 @@
 package com.example.gdms_front
 
 import android.content.Intent
+import android.graphics.BitmapFactory
+import android.graphics.Matrix
 import android.os.Bundle
 import android.os.Handler
 import android.widget.ImageView
@@ -8,6 +10,8 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.bumptech.glide.Glide
+import com.example.gdms_front.auth.AspectRatioImageView
 import com.example.gdms_front.auth.IntroActivity
 import kotlin.random.Random
 
@@ -17,20 +21,8 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        val imageView: ImageView = findViewById(R.id.backgroundImageView)
-
-        val images = arrayOf(
-            R.drawable.background1,
-            R.drawable.background2,
-         //   R.drawable.background3
-            // 필요한 만큼 추가
-        )
-
-        // 랜덤으로 이미지 선택
-        val randomImage = images[Random.nextInt(images.size)]
-
-        // ImageView에 랜덤 이미지 설정
-        imageView.setImageResource(randomImage)
+        val imageView = findViewById<AspectRatioImageView>(R.id.backgroundImageView)
+        imageView.setImageResource(R.drawable.splash_img)
 
         Handler().postDelayed({
             startActivity((Intent(this, IntroActivity::class.java))) //인트로엑티비티로 이동
